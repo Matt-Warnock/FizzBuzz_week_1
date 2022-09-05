cad572a3e4a0f27638bc23bded28f8cb05ae3325
S-DRE <sdreworks@gmail.com>
2022-09-05 11:23:41 +0200

    Refactoring: Tests 1, 2, 4

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,13 +1,5 @@
 public class FizzBuzz {
     public static String convert(int i) {
-        if (i == 2) {
-            return "2";
-        }
-
-        if (i == 4) {
-            return "4";
-        }
-
-        return "1";
+        return String.valueOf(i);
     }
 }
